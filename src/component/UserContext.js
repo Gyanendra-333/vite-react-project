b83ea644d5bf1c5e3e4201e9a2dfcb7042691f23
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-23 18:20:59 +0530

    context

--- src/component/UserContext.js
+++ src/component/UserContext.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 const UserContext = React.createContext()
+function UserContext() {
+    return (
+        <>
+            <h1>Context API</h1>
+        </>
+    )
+}
 
 export default UserContext;
