b3006d752b19f31e5b0af1020c73f7d9630535c9
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-27 14:26:43 +0530

    js

---
+++ HTML_Test/index.js
@@ -0,0 +1,22 @@
+
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.style.scrollBehavior = "auto";
+    scrollContainer.scrollLeft += evt.deltaY;
+});
+
+// Btn Click 
+
+nextBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
+});
